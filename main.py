c60dbd473195cf387f40c77c19a0ee061907e693
hllqk <97894459+hllqk@users.noreply.github.com>
2022-08-12 13:14:01 +0000

    by python flask

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from include.index import *
 app = Flask('app')
-
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('index.html')
+@app.route('/result',methods=['POST'])
+def result():
+  r=request.form['readme']
+  translate(r)
+  #print('翻译完成，前往out.txt查看')
+  return render_template('out.html')
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8080,debug = True)
